cce10dfee3c9462779f0d7f2cdbea1ae1376b408
seungyong <gosng2001@naver.com>
2021-10-16 18:33:33 +0900

    Update: webpack version 5 upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
 var path = require('path');
 
 module.exports = {
-	mode: 'production',
-	entry: './js/app.js',
-	output: {
-			path: path.resolve(__dirname, 'build'),
-			filename: 'main.bundle.js'
-	},
-	module: {
-		rules: [{
-			test: /\.m?js$/,
-			exclude: /(node_modules|bower_components)/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env']
-				}
-			}
-		}]
-	},
-	stats: {
-			colors: true
-	},
-	devtool: 'source-map'
-};
+  mode: 'none',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+}
